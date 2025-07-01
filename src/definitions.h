845456d5056062705a004637e3d3526b70f93ab1
abhijit-031991 <info.arcturus.inc@gamil.com>
2025-07-01 11:55:57 +0530

    Add initial project files and documentation for header and library usage

---
+++ src/definitions.h
@@ -0,0 +1,16 @@
+// Firmware Version //
+const float firmwareVersion = 1.0;
+
+// Pin Definitions //
+#define GPS_TX PC1
+#define GPS_RX PC0
+#define GPS_EN PB5
+#define SCL_PIN PB15
+#define SDA_PIN PA15
+#define MOSI_PIN PA10
+#define MISO_PIN PB14
+#define SCK_PIN PB13
+#define FSS_PIN PB9
+#define ACC_INT1_PIN PB4
+#define ACC_INT2_PIN PB3
+#define CS_INT_PIN PB0
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();        // join i2c bus (address optional for master)
+  Wire.setSCL(PB15);
+  Wire.setSDA(PA15);
+  Serial1.begin(115200);
+  Serial1.println("\nI2C Sniffer");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  byte error, address;
+  int nDevices;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial1.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmission to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial1.print("I2C device found at address 0x");
+      if (address<16)
+        Serial1.print("0");
+      Serial1.print(address,HEX);
+      Serial1.println(" !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial1.print("Unknown error at address 0x");
+      if (address<16)
+        Serial1.print("0");
+      Serial1.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial1.println("No I2C devices found\n");
+  else
+    Serial1.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
 }
